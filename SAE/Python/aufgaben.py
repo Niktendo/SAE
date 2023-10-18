6ce4b9f9dc72e6c832d2a33f49404c7e09201dd9
Niklas Jeske <31368200+Niktendo@users.noreply.github.com>
2023-10-18 12:26:45 +0200

    Add files via upload

--- SAE/Python/aufgaben.py
+++ SAE/Python/aufgaben.py
@@ -11,6 +11,26 @@ def aufgabe2():
     print("OUTPUT:", "Hallo, " + name)
 
 def aufgabe3():
+    '''
+    Typumwandlung
+    Automatische / Implizite Typkonversion
+    Eine Operation mit unterschiedlichen aber kompatiblen Typen liefert ein Ergebnis des  mächtigeren Typs
+    '''
+    #print(1 + 1.0) # Zwei Zahlen funktioniert
+    #print("1" + 1.0) # String + Zahl funktioniert nicht
+    '''
+    Erzwungene / Explizite Typkonversion
+    Schreibt man Datentyp (Wert), versucht Python Wert mit dem gewünschten Datentyp  zu liefern.
+    '''
+    #print(int("1") + 0.0)
+    #print(str(1) + "1")
+    '''
+    Eingaben von der Tastatur sind IMMER erstmal Strings, sie müssen zunächst mit der erzw. Typumwandung behandelt werden.
+    '''
+    eingabe = input("Gib eine Zahl ein:\n")
+    print((int(eingabe)) + 88)
+
+def aufgabe4():
     #Aufgabe 3: Gib zwei Zahlen ein
     print("\nAufgabe 3")
     i1 = 0
@@ -19,7 +39,7 @@ def aufgabe3():
     i2 = input("Gebe die zweite Zahl ein: ")
     print("OUTPUT:", (int(i1)) + (int(i2)))
 
-def aufgabe4():
+def aufgabe5():
     #Aufgabe 4: Exponenten u.a.
     print("\nAufgabe 4")
     basis = float(input("Basis: "))
@@ -27,7 +47,7 @@ def aufgabe4():
     #print(basis**exponent) # oder andere Methode                
     print(f"{basis**exponent:.2f}") # .2f steht für float also Kommazahlen                
 
-def aufgabe5():
+def aufgabe6():
     #Aufgabe 5: Datum ausgeben, mehrere Methoden
     print("\nAufgabe 5")
     tag = input("Tag: ")
@@ -37,13 +57,12 @@ def aufgabe5():
     #print(tag + "." + monat + "." + jahr) # Andere Methode
     print(f"{tag:0>2}.{monat:0>2}.{jahr}") # Müssen wir nicht wissen
 
-def aufgabe6():
+def aufgabe7():
     #Aufgabe 6: If-Cases
     '''
     Schreibe ein Programm, welches einen eingegeben Namen
     mit "Hallo" + name beantwortet.
-    falls der Name aber "Thiersch" ist, sagt es
-    "Hallo Meister".
+    Falls der Name aber "Thiersch" ist, sagt es "Hallo Meister".
     '''
     print("\nAufgabe 6")
     #name = input("Name: ")
@@ -57,5 +76,8 @@ def aufgabe6():
     if name == "Niklas":
         name = "Meister"
     print("Hallo", name)
-
-aufgabe6()
+    '''
+    Mit if wird ein Vergleich eingeleitet. Ergiebt dieser "true", so wird der folgende Codeblock ausgeführt.
+    Wenn nicht, wird dieser übersprungen.
+    '''
+aufgabe7()
