aa2c9e7e866c248b45deb79457b7c2b7d18abde9
Niklas Jeske <31368200+Niktendo@users.noreply.github.com>
2023-11-07 07:43:27 +0100

    Add files via upload

--- SAE/Python/aufgaben.py
+++ SAE/Python/aufgaben.py
@@ -11,6 +11,8 @@ def aufgabe2():
     print("OUTPUT:", "Hallo, " + name)
 
 def aufgabe3():
+    #Aufgabe 3: Typumwandlung
+    print("\nAufgabe 3")
     '''
     Typumwandlung
     Automatische / Implizite Typkonversion
@@ -31,8 +33,8 @@ def aufgabe3():
     print((int(eingabe)) + 88)
 
 def aufgabe4():
-    #Aufgabe 3: Gib zwei Zahlen ein
-    print("\nAufgabe 3")
+    #Aufgabe 4: Gib zwei Zahlen ein
+    print("\nAufgabe 4")
     i1 = 0
     i2 = 0
     i1 = input("Gebe die erste Zahl ein: ")
@@ -40,16 +42,16 @@ def aufgabe4():
     print("OUTPUT:", (int(i1)) + (int(i2)))
 
 def aufgabe5():
-    #Aufgabe 4: Exponenten u.a.
-    print("\nAufgabe 4")
+    #Aufgabe 5: Exponenten u.a.
+    print("\nAufgabe 5")
     basis = float(input("Basis: "))
     exponent = float(input("Exponent: "))
     #print(basis**exponent) # oder andere Methode                
     print(f"{basis**exponent:.2f}") # .2f steht für float also Kommazahlen                
 
 def aufgabe6():
-    #Aufgabe 5: Datum ausgeben, mehrere Methoden
-    print("\nAufgabe 5")
+    #Aufgabe 6: Datum ausgeben, mehrere Methoden
+    print("\nAufgabe 6")
     tag = input("Tag: ")
     monat = input("Monat: ")
     jahr = input("Jahr: ")
@@ -58,13 +60,13 @@ def aufgabe6():
     print(f"{tag:0>2}.{monat:0>2}.{jahr}") # Müssen wir nicht wissen
 
 def aufgabe7():
-    #Aufgabe 6: If-Cases
+    #Aufgabe 7: If-Cases
     '''
     Schreibe ein Programm, welches einen eingegeben Namen
     mit "Hallo" + name beantwortet.
     Falls der Name aber "Thiersch" ist, sagt es "Hallo Meister".
     '''
-    print("\nAufgabe 6")
+    print("\nAufgabe 7")
     #name = input("Name: ")
     #if name == "Niklas":
     #    print("Hallo Meister")
@@ -80,4 +82,46 @@ def aufgabe7():
     Mit if wird ein Vergleich eingeleitet. Ergiebt dieser "true", so wird der folgende Codeblock ausgeführt.
     Wenn nicht, wird dieser übersprungen.
     '''
-aufgabe7()
+def aufgabe8():
+    #Aufgabe 8: Größte Zahl ausgeben
+    print("\nAufgabe 8")
+    '''
+    Aufgabe:
+    Schreibe ein Programm, das zwei Zahlen einliest und
+    die Größte ausgibt.
+    '''
+    imax = ''
+    i1 = float(input("Zahl 1: "))
+    i2 = float(input("Zahl 2: "))
+    print("OUTPUT:")
+    if i1 == i2:
+        print("Beide Zahlen sind gleich groß.")
+    elif i1 < i2:
+        imax = i2
+    else:
+        imax = i1
+    print(imax)
+
+    imax = 0
+    i1 = float(input("Zahl 1: "))
+    i2 = float(input("Zahl 2: "))
+    i3 = float(input("Zahl 3: "))
+    if i1 > i2:
+        if i1 > i3:
+            imax = i1
+        else:
+            if i3 > i2:
+                imax = i3
+    else:
+        if i2 > i3:
+            imax = i2
+        else:
+            imax = i3
+    print("OUTPUT:")
+    print(imax)
+
+'''
+Die Verschachtelung im unteren Beispiel lässt sich durch else if (elif) lösen.
+'''
+
+aufgabe8()
